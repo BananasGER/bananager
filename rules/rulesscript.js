fef253dec37c9c865e7b59277986837a2cd0308e
BananasGER <bananaofficial.twitch@gmail.com>
2025-01-21 00:37:02 +0100

    Add files via upload

--- rules/rulesscript.js
+++ rules/rulesscript.js
@@ -2,3 +2,16 @@ function toggleGroup(groupId) {
     const group = document.getElementById(groupId);
     group.classList.toggle("open");
 }
+
+function toggleLanguage() {
+    const germanBox = document.getElementById("lang-de");
+    const englishBox = document.getElementById("lang-en");
+
+    if (germanBox.classList.contains("active")) {
+        germanBox.classList.remove("active");
+        englishBox.classList.add("active");
+    } else {
+        germanBox.classList.add("active");
+        englishBox.classList.remove("active");
+    }
+}
